2c64056c1c149b9e9b3498994e7c5ec1aae72527
aseemar <ajseemar@gmail.com>
2019-07-17 12:54:53 -0700

    create input manager to handle keyboard input for player movement

---
+++ src/utils/input.js
@@ -0,0 +1,54 @@
+class InputManager {
+    constructor() {
+        this.pressedKeys = {};
+
+        document.addEventListener('keydown', e => this.setKey(e, true));
+        document.addEventListener('keyup', e => this.setKey(e, false));
+    }
+
+    setKey(e, status) {
+        e.preventDefault();
+        let key;
+        switch (e.keyCode) {
+            case 32:
+                key = KEYS.SPACE;
+                break;
+            case 65:
+                key = KEYS.LEFT;
+                break;
+            case 87:
+                key = KEYS.UP;
+                break;
+            case 68:
+                key = KEYS.RIGHT;
+                break;
+            case 83:
+                key = KEYS.DOWN;
+                break;
+            case 37:
+                key = KEYS.LEFT;
+                break;
+            case 38:
+                key = KEYS.UP;
+                break;
+            case 39:
+                key = KEYS.RIGHT;
+                break;
+            case 40:
+                key = KEYS.DOWN;
+                break;
+            default:
+                // Convert ASCII codes to letters
+                key = String.fromCharCode(e.keyCode);
+
+        }
+
+        this.pressedKeys[key] = status;
+    }
+
+    isPressed(key) {
+        return this.pressedKeys[key];
+    }
+}
+
+module.exports = InputManager;
---
+++ src/utils/utils.js
@@ -0,0 +1,20 @@
+class Utilities {
+    index(i, j, rows) {
+        return i + (j * rows);
+    }
+
+    clamp(num, min, max) {
+        return Math.max(min, Math.min(num, max))
+    }
+
+    heuristic(a, b) {
+        const dx = b.position.x - a.position.x;
+        const dy = b.position.y - a.position.y;
+        return Math.sqrt(dx * dx + dy * dy);
+        // return Math.abs(dx) + Math.abs(dy);
+    };
+
+    map(num, x1, y1, x2, y2) {
+        return (num - x1) * (y2 - x2) / (y1 - x1) + x2;
+    }
+}
